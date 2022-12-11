b7e23caaa755c061b17ec6e6083fec88d8a3b527
Utkarsh Padia <utkarshpadia@192.168.1.18>
2022-12-12 00:43:03 +0530

    api

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tweet-analysis
-This project is a web application that predicts and visualises the analytical information regarding any trend on twitter
+
+This project is a web application that can predict and visualise the analytical information regarding any trend on [Twitter](https://twitter.com/home)
+
+
+## Introduction
+
+The objective of this application is to provide an analysis on any trending topics on twitter. The tweets in English language on any topic are fetched using the [Twitter API](https://developer.twitter.com/en/docs/twitter-api). The following analytical information is visualised in the application for the searched topic
+
+1. The change in the number of tweets in a day or a week.
+2. The current sentiment of the Tweeter's towards the topic. The sentiment is identified using a RandomClassifierModel trained on the data from [Kaggle](https://www.kaggle.com/datasets/saurabhshahane/twitter-sentiment-dataset).
+3. The change in the average sentiment of the Tweeter's towards the topic over time.
+4. The average tweets per a single user on the topic.
+
+
+## Usage
+
+### Installation
+
+The Back-end code is built on Python 3.9 and the Front-end is built on Angular 14.2.7.. The IDE used in development of the back-end code is [PyCharm](https://www.jetbrains.com/pycharm/) and for the front-end it is [Visual Studio Code](https://code.visualstudio.com). The same IDE's can be used for the running the application by the users or any other IDE's supporting the mentioned environments.
+
--- app.py
+++ app.py
@@ -1,14 +1,90 @@
 from flask import Flask, request
 from flask_cors import CORS
 import requests
+import json
+import pandas as pd
+import pickle
+import nltk
+import re
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from langdetect import detect
+
 
 app = Flask(__name__)
 CORS(app)
 
+
+def tokenize(text):
+    '''
+
+    This function processes the text by removing stopwords, tokenizing the sentences and lemmatizing words to
+    make the text usable for further transformation
+
+    Input:
+    text - a string(tweet) which is the input of the model
+
+    Output:
+    returns the input text as clean tokens in a list
+
+
+    '''
+
+    tokens = word_tokenize(text.lower())
+    tokens = [word for word in tokens if word not in stopwords.words('english')]
+    lemmatizer = WordNetLemmatizer()
+    tokenized_text = [lemmatizer.lemmatize(token).strip() for token in tokens]
+    return tokenized_text
+
+def tweet_cleaner(text):
+
+    '''
+
+    This function is used to clean the tweets fetched from the API. It removes the URL's, user mentions
+    and # special character.
+
+    Input:
+    text - a string(tweet) for which the sentiment is to be predicted
+
+    Output:
+    cleaned_text - returns the input string in a cleaned format.
+
+    '''
+
+
+    text = re.sub('http://\S+|https://\S+', '', text)
+    cleaned_text = text[text.find(":")+1:]
+    cleaned_text = cleaned_text.replace("#","")
+    split_arr = cleaned_text.split()
+    cleaned_arr =[]
+    for i in range(len(split_arr)):
+        if (split_arr[i].find("@") == -1):
+            cleaned_arr.append(split_arr[i])
+    cleaned_text = " ".join(cleaned_arr)
+
+    return cleaned_text
+
 @app.route('/get_tweets',methods = ['POST'])
 def get_tweet():
     hastag = request.get_json()
-    print (hastag)
+    token_object = open("tokens.txt","r")
+    access_token = token_object.readline()
+    token_object.close()
+    query = "https://api.twitter.com/2/tweets/search/recent?query=%23"+hastag['hashtag']+"&tweet.fields=created_at&max_results=100&expansions=author_id&user.fields=created_at"
+    x = requests.get(query , headers={"Authorization":"Bearer "+access_token})
+
+    tweets = pd.DataFrame(x.json()['data'])
+    tweets['language'] = tweets['text'].apply(lambda x: detect(x))
+    tweets = tweets.loc[tweets['language'] == "en"]
+    f = open("model.pkl", 'rb')
+    predict_model = pickle.load(f)
+    tweets['text'] = tweets['text'].apply(tweet_cleaner)
+    sentiments = predict_model.predict(tweets['text'])
+    tweets['sentiment'] = sentiments
+    print(tweets['sentiment'].value_counts())
     return '200'
 
 if (__name__=='__main__'):
